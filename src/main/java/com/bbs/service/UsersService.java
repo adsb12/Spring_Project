43b501b91b227b5a4977c9f7c11fe12a4c1688db
adsb12 <wjswk520gmail.com>
2021-11-25 09:45:59 +0900

    2021-11-25 commit

---
+++ src/main/java/com/bbs/service/UsersService.java
@@ -0,0 +1,5 @@
+package com.bbs.service;
+
+public interface UsersService {
+	public int check_id(String user_id) throws Exception;
+}
---
+++ src/main/java/com/bbs/service/UsersServiceImpl.java
@@ -0,0 +1,13 @@
+package com.bbs.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsersServiceImpl implements UsersService {
+
+	@Override
+	public int check_id(String user_id) throws Exception {
+		return 0;
+	}
+
+}
