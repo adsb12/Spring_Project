189b472e2b9e353e4ff24bff4fa3388f9ddc4e23
adsb12 <wjswk520gmail.com>
2021-11-29 10:23:21 +0900

    2021-11-29 commit

--- src/main/java/com/bbs/controller/MainController.java
+++ src/main/java/com/bbs/controller/MainController.java
@@ -100,4 +100,12 @@ public class MainController {
 		return url;
 	}
 	
+	@RequestMapping(value = "/logout", method = RequestMethod.GET)
+	public String logout(HttpSession session) throws Exception {
+		
+		session.invalidate();
+		
+		return "redirect:/";
+	}
+	
 }
